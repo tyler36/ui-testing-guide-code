522d12a1c4303db4208e7a0891237e2b88d51d4e
tyler36 <tyler36@users.noreply.github.com>
2024-10-02 16:37:20 +0900

    testing interactions (#4)

--- src/InboxScreen.stories.jsx
+++ src/InboxScreen.stories.jsx
@@ -1,6 +1,7 @@
 import { http, HttpResponse } from 'msw'
 import InboxScreen from './InboxScreen'
 import { Default as TaskListDefault } from './components/TaskList.stories'
+import { expect, userEvent, findByRole, within } from '@storybook/test'
 
 export default {
   component: InboxScreen,
@@ -23,5 +24,59 @@ export const Error = {
     msw: {
       handlers: [http.get('/tasks', () => HttpResponse.json([]))],
     },
+  },
+}
+
+export const PinTask = {
+  parameters: {
+    ...Default.parameters,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    const getTask = (id) => canvas.findByRole('listitem', { name: id })
+    const itemToPin = await getTask('task-4')
+
+    // Find the pin button
+    const pinButton = await findByRole(itemToPin, 'button', { name: 'pin' })
+
+    // Click button
+    await userEvent.click(pinButton)
+
+    // Check that the pin button is now an unpin button
+    const unpinButton = within(itemToPin).getByRole('button', { name: 'unpin' })
+    await expect(unpinButton).toBeInTheDocument()
+  },
+}
+
+export const ArchiveTask = {
+  parameters: {
+    ...Default.parameters,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    const getTask = (id) => canvas.findByRole('listitem', { name: id })
+
+    const itemToArchive = await getTask('task-2')
+    const archiveButton = await findByRole(itemToArchive, 'button', {
+      name: 'archiveButton-2',
+    })
+
+    await userEvent.click(archiveButton)
+  },
+}
+
+export const EditTask = {
+  parameters: {
+    ...Default.parameters,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    const getTask = (id) => canvas.findByRole('listitem', { name: id })
+
+    const itemToEdit = await getTask('task-5')
+    const taskInput = await findByRole(itemToEdit, 'textbox')
+    await userEvent.type(taskInput, ' and disable state')
+    await expect(taskInput.value).toBe('Fix bug in input error state and disable state')
   },
 }
