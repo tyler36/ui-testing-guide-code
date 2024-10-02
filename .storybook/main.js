81757ed0f4677d7c888a4cffd8d0d74d9e190afa
tyler36 <tyler36@users.noreply.github.com>
2024-10-02 17:08:11 +0900

    add accessibility tests (#5)
    * add accessibility tests
    
    * fix archive contrast level
    
    * resolve "Required ARIA parent role not present: list"
    
    * add AXE to test-runner

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,16 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  staticDirs: ["../public"],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  staticDirs: ['../public'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: '@storybook/react-vite',
     options: {},
   },
-};
-export default config;
+}
+export default config
