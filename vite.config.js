76c3657a48cced75ad17cdd4cb757c0c9e4d93b4
tyler36 <7234392+tyler36@users.noreply.github.com>
2024-10-07 12:28:58 +0900

    ⚙️ DDEV vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const port = 5173
+const origin = `${process.env.DDEV_PRIMARY_URL}:${port}`
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: port,
+    origin: origin,
+    strictPort: true,
+  },
   plugins: [react()],
 })
