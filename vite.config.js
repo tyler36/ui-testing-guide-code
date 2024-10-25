e345065b906a29ebeaa0432a5e2f539d061f5248
tyler36 <tyler36@users.noreply.github.com>
2024-10-25 10:42:05 +0900

    DDEV (#10)
    * ⚙️ DDEV config
    
    * ⚙️ DDEV vite
    
    * ddev-cypress
    
    * storybook
    
    ---------
    
    Co-authored-by: tyler36 <7234392+tyler36@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+let customConfig = {
   plugins: [react()],
-})
+}
+
+if (process.env.DDEV_PRIMARY_URL) {
+  const port = 5173
+  const origin = `${process.env.DDEV_PRIMARY_URL}:${port}`
+
+  customConfig.server = {
+    host: '0.0.0.0',
+    port: port,
+    origin: origin,
+    strictPort: true,
+  }
+}
+
+
+// https://vitejs.dev/config/
+export default defineConfig(customConfig)
